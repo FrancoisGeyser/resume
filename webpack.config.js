03aa2b6ffd01f3d4b8e9bf4a08645aa7576eadba
francoisgeyser <fgprojects@pm.me>
2022-09-23 20:22:05 +0300

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: ['@babel/polyfill', './src/index'],
+  entry: ['./src/index'],
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
